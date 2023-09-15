f4d3829405d279a5481a309e4757367f9c596e11
KabildevPitchai <137326813+KabildevPitchai@users.noreply.github.com>
2023-09-15 12:52:07 +0530

    Add files via upload

--- For_loop.java
+++ For_loop.java
@@ -1,86 +1,88 @@
-package looping_statements;
+package Methods;
 
 public class For_loop {
 
 	public static void main(String[] args) {
-		For_loop f=new For_loop();
-		f.num_pattern();
-		f.left_triangle();
-		f.right_triangle();
-		f.triangle();
-		f.testPattern();
-		f.fivedecrease_pattern();
- 		f.number_row();
-	}
-
-	private void number_row() {
-		for(int row=5;row>=1;row--) {
-			for(int col=1;col<=row;col++) {
-				System.out.print(row);
-			}System.out.println();
-
-		}System.out.println();
-	}
-
-	private void fivedecrease_pattern() {
-		for(int row=1;row<=5;row++) {
-			for(int col=5;col>=row;col--) {
-				System.out.print(col);
-			}System.out.println();
-		}System.out.println();
-	}
-
-	private void triangle() {
-		for(int i=1;i<=5;i++) {
-			for(int j=1;j<=i;j++) {
-				System.out.print(j);
-			}System.out.println();
+		// TODO Auto-generated method stub
+		for(int no=1;no<=5;++no) {
+			System.out.println(no);
 		}
-		for(int row=5;row>=1;row--) {
-			for(int col=1;col<=row;col++) {
-				System.out.print(col);
-			}System.out.println();
+		
+		int no1=1;
+		while(no1<=5) {
+			System.out.println(no1);
+			no1++;
 		}
-		System.out.println();
-	}
+		System.out.println("hi"+no1);
+		
+		int no2=1;
+		for (;no2<=6;no2++) {
+			System.out.println(no2);   //fixing error below the program
+		}System.out.println("hello"+no2);
+		
+		/*for (int no2=1;no2<=6;no2++) {
+			System.out.println(no2);
+		}System.out.println("hello"+no2)*/
+		
+		//COMMON DIVISORS PROGRAM
 
-	private void right_triangle() {
-		for(int row=1;row<=5;row++) {
-			for(int col=5;col>=row;col--) {
-				System.out.print(col);
-			}System.out.println();
-		}System.out.println();
-	}
-
-	private void left_triangle() {
-		for(int row=1;row<=5;row++) {
-			for(int col=1;col<=row;col++) {
-				System.out.print(col+" ");
-			}System.out.println();
-		}System.out.println();
-	}
-
-	private void num_pattern() {
-		for(int row=1;row<=5;row++) {
-			for(int col=1;col<=row;col++) {
-				System.out.print(col);
-			}System.out.println();
-		}System.out.println();
-	}
-
-	private void testPattern() {
-		int sum = 0;
-		for (int row = 5; row <= 9; row++) {
-			System.out.print(row + " ");
+		int val=100,val1=60;
+		int div=2;int small=val<val1?val:val1;
+		
+//		if(val<val1)
+//			small=val;
+//		else
+//			small=val1;
+		
+		while(div<=small) {
+			if(val%div==0 && val1%div==0) 
+			{
+				System.out.println(div);
+			}
+			div++;
 		}
-		System.out.println();
-		System.out.println();
-		for (int row = 5; row <= 9; row++) {
-			System.out.println(row + " ");
-			sum = sum + row;
+		
+		//GREAT COMMON DIVISER PROGRAM
+		
+		int val2=100,val3=60;
+		int small1=val2<val3?val2:val3;
+		int gcd=0;
+		for(int div1=2;div1<=small1;div1++) {
+			if(val2%div1==0 && val3%div1==0) 
+			{
+				gcd=div1;
+			}
 		}
-		System.out.println();
-		System.out.println("Sum value is : " + sum);
-		System.out.println();
+		if(gcd!=0)
+		System.out.println("GCD =>"+gcd);
+		else
+			System.out.println("no common divisors found");
+
+	
+	
+	//LEAST COMMON MULTIPLE PROGRAM
+	
+	int n1=3;
+	int n2=9;
+	int big=n1>n2?n1:n2;
+	while(true) {
+		if(big%n1==0 && big%n2==0) {
+		System.out.println("LCM IS =>"+big);	
+		break;
+		}
+		big++;
 	}
+	
+	
+	
+	
+	
 }
+}
+
+
+
+//FOR LOOP 
+//1.INITIAL-only one
+//2.CONDITION-every time before the action
+//3.INCREMENT/DECREMENT-every time after the action
